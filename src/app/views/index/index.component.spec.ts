127617084bb4ecdfe811c744102d5468021e8610
Lucilor <lucilor@me.com>
2020-12-23 17:36:15 +0800

    ✨feat(index): link to kodexplorer

--- src/app/views/index/index.component.spec.ts
+++ src/app/views/index/index.component.spec.ts
@@ -1,5 +1,6 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
 import {RouterTestingModule} from "@angular/router/testing";
 
 import {IndexComponent} from "./index.component";
@@ -11,7 +12,7 @@ describe("IndexComponent", () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [IndexComponent],
-            imports: [MatButtonModule, RouterTestingModule]
+            imports: [MatButtonModule, MatDividerModule, RouterTestingModule]
         }).compileComponents();
     });
 
