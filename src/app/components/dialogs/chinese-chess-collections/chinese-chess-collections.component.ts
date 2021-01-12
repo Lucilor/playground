5d87bf5ce4ad048bcb48f391a9de2b45266ed188
Lucilor <lucilor@me.com>
2021-01-12 19:51:54 +0800

    🐞fix(ui): bugs

--- src/app/components/dialogs/chinese-chess-collections/chinese-chess-collections.component.ts
+++ src/app/components/dialogs/chinese-chess-collections/chinese-chess-collections.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component} from "@angular/core";
 import {MatDialogRef} from "@angular/material/dialog";
 import {timeout} from "@lucilor/utils";
 import {HttpService} from "@src/app/modules/http/services/http.service";
+import {NgxUiLoaderService} from "ngx-ui-loader";
 import {getOpenDialogFunc} from "../dialog.common";
 
 type Collections = {name: string; url: string}[];
@@ -15,11 +16,17 @@ export class ChineseChessCollectionsComponent implements AfterViewInit {
     loadId = "ChineseChessCollectionsLoader";
     data: Collections = [];
 
-    constructor(public dialogRef: MatDialogRef<ChineseChessCollectionsComponent, null>, private http: HttpService) {}
+    constructor(
+        public dialogRef: MatDialogRef<ChineseChessCollectionsComponent, null>,
+        private http: HttpService,
+        private loader: NgxUiLoaderService
+    ) {}
 
     async ngAfterViewInit() {
         await timeout(0);
+        this.loader.startLoader(this.loadId);
         const response = await this.http.get<Collections>("static/chinese-chess/getCollections.php");
+        this.loader.stopLoader(this.loadId);
         if (response?.data) {
             this.data = response.data;
         }
