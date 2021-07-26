51fd97ac9b9666133204c9acd8edbeb4beea3d96
Lucilor <lucilor@me.com>
2021-07-26 14:59:44 +0800

    ✨ feat(music-player): playlists animations

--- src/app/modules/music-player/components/playlists/playlists.component.ts
+++ src/app/modules/music-player/components/playlists/playlists.component.ts
@@ -1,56 +1,97 @@
-import {ChangeDetectorRef, Component, Input, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 import {MatSelectionListChange} from "@angular/material/list";
+import {timeout} from "@lucilor/utils";
 import {MusicService} from "@modules/music-player/services/music.service";
 import {Playlist, PlaylistDetail, Track} from "@modules/music-player/services/netease-music.types";
 import {AppStatusService} from "@services/app-status.service";
+import {fadeInUpOnEnterAnimation, fadeOutDownOnLeaveAnimation} from "angular-animations";
 import Color from "color";
 
 @Component({
     selector: "app-playlists",
     templateUrl: "./playlists.component.html",
-    styleUrls: ["./playlists.component.scss"]
+    styleUrls: ["./playlists.component.scss"],
+    animations: [
+        fadeInUpOnEnterAnimation({anchor: "enter", duration: 500, delay: 100}),
+        fadeOutDownOnLeaveAnimation({anchor: "leave", duration: 500, delay: 100})
+    ]
 })
 export class PlaylistsComponent implements OnInit {
     @Input() mainColor = new Color("white");
-    playlists: Playlist[] = [];
-    page = 1;
     limit = 10;
-    maxPage = 0;
+    playlistsPage = 0;
+    playlistsMaxPage = 0;
+    tracksPage = 0;
+    tracksMaxPage = 0;
+    playlists: Playlist[] = [];
     playlist: PlaylistDetail | null = null;
+    allTracks: Track[] = [];
+    track: Track | null = null;
     loaderId = "music-player-playlists";
     loading = false;
+    level = 0;
     get btnStyle(): Partial<CSSStyleDeclaration> {
-        return {color: this.mainColor.negate().string()};
+        return {color: this.mainColor.isLight() ? "black" : "white"};
     }
 
-    constructor(private music: MusicService, private status: AppStatusService, private cd: ChangeDetectorRef) {}
+    constructor(private music: MusicService, private status: AppStatusService) {}
 
     async ngOnInit() {
-        this.maxPage = Math.ceil((await this.music.getPlaylistCount()) / this.limit);
-        await this.nextPage();
+        this.playlistsMaxPage = this._getMaxPage(await this.music.getPlaylistCount());
+        await this.playlistsNextPage();
     }
 
-    async nextPage() {
-        const {page, limit, maxPage} = this;
-        if (page > maxPage || this.loading) {
+    private _getMaxPage(total: number) {
+        return Math.ceil(total / this.limit);
+    }
+
+    async playlistsNextPage() {
+        if (this.playlistsPage > this.playlistsMaxPage || this.loading) {
             return;
         }
+        this.playlistsPage++;
+        const {limit, playlistsPage} = this;
         this.loading = true;
-        const playlists = await this.music.getPlaylists(page, limit);
+        console.log(playlistsPage);
+        const playlists = await this.music.getPlaylists(playlistsPage, limit);
         playlists.forEach((playlist) => this.playlists.push(playlist));
         this.loading = false;
-        this.page++;
+    }
+
+    tracksNextPage() {
+        this.tracksPage++;
+        const {limit, tracksPage, playlist} = this;
+        if (this.tracksPage > this.tracksMaxPage || !playlist) {
+            return;
+        }
+        playlist.tracks = this.allTracks.slice(0, tracksPage * limit);
     }
 
     async onPsYReachEnd() {
-        await this.nextPage();
+        switch (this.level) {
+            case 0:
+                await this.playlistsNextPage();
+                break;
+            case 1:
+                this.tracksNextPage();
+                break;
+            default:
+                break;
+        }
     }
 
     async selectPlaylist(event: MatSelectionListChange) {
         const id: Playlist["id"] = event.options[0].value;
         this.status.startLoader({id: this.loaderId});
-        this.playlist = await this.music.getPlaylistDetail(id);
+        const playlist = await this.music.getPlaylistDetail(id);
+        this.playlist = playlist;
         this.status.stopLoader();
+        await timeout(0);
+        this.level++;
+        this.allTracks = playlist ? playlist.tracks : [];
+        this.tracksMaxPage = this._getMaxPage(this.allTracks.length);
+        this.tracksPage = 0;
+        this.tracksNextPage();
     }
 
     getTrackSubtitle(track: Track) {
@@ -65,6 +106,14 @@ export class PlaylistsComponent implements OnInit {
             return;
         }
         const tracks = await this.music.getTracks([id]);
-        console.log(tracks);
+        if (tracks) {
+            this.track = tracks[0];
+        }
+        await timeout(0);
+        this.level++;
+    }
+
+    back() {
+        this.level--;
     }
 }
