cc33b19e05367c53eebc0721b103831f1fa24f81
Lucilor <lucilor@me.com>
2022-11-16 17:07:15 +0800

    🔧 build(deps): update deps

--- src/app/views/happy-birthday/happy-birthday.component.spec.ts
+++ src/app/views/happy-birthday/happy-birthday.component.spec.ts
@@ -19,7 +19,7 @@ describe("HappyBirthdayComponent", () => {
         fixture.detectChanges();
     });
 
-    fit("should create", () => {
+    it("should create", () => {
         expect(component).toBeTruthy();
     });
 });
